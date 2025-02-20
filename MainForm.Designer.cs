ebdba5e50dd8b37abaf9fadd0310cdecf75440fc
Jacqueline Jimenez <jacquelinejp2019@gmail.com>
2025-02-20 18:46:07 +0100

    MainForm modified and new menu

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,12 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.sidebarPanel = new System.Windows.Forms.Panel();
+            this.btnDashboard = new System.Windows.Forms.Button();
+            this.btnIncomeManagement = new System.Windows.Forms.Button();
+            this.btnExpensesManagement = new System.Windows.Forms.Button();
+            this.btnSavings = new System.Windows.Forms.Button();
+            this.btnReports = new System.Windows.Forms.Button();
+            this.btnCategoryList = new System.Windows.Forms.Button();
+            this.picDarkMode = new System.Windows.Forms.PictureBox();
+            this.picHelp = new System.Windows.Forms.PictureBox();
+            this.picSettings = new System.Windows.Forms.PictureBox();
+            this.topPanel = new System.Windows.Forms.Panel();
+            this.leftCornerLogo = new System.Windows.Forms.PictureBox();
+            this.mainFormTitle = new System.Windows.Forms.Label();
+            this.sidebarPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picDarkMode)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHelp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSettings)).BeginInit();
+            this.topPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.leftCornerLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sidebarPanel
+            // 
+            this.sidebarPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(61)))), ((int)(((byte)(98)))));
+            this.sidebarPanel.Controls.Add(this.topPanel);
+            this.sidebarPanel.Controls.Add(this.picSettings);
+            this.sidebarPanel.Controls.Add(this.picHelp);
+            this.sidebarPanel.Controls.Add(this.picDarkMode);
+            this.sidebarPanel.Controls.Add(this.btnCategoryList);
+            this.sidebarPanel.Controls.Add(this.btnReports);
+            this.sidebarPanel.Controls.Add(this.btnSavings);
+            this.sidebarPanel.Controls.Add(this.btnExpensesManagement);
+            this.sidebarPanel.Controls.Add(this.btnIncomeManagement);
+            this.sidebarPanel.Controls.Add(this.btnDashboard);
+            this.sidebarPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidebarPanel.Location = new System.Drawing.Point(0, 0);
+            this.sidebarPanel.Name = "sidebarPanel";
+            this.sidebarPanel.Size = new System.Drawing.Size(394, 1099);
+            this.sidebarPanel.TabIndex = 0;
+            // 
+            // btnDashboard
+            // 
+            this.btnDashboard.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDashboard.FlatAppearance.BorderSize = 0;
+            this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDashboard.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDashboard.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(244)))), ((int)(((byte)(246)))));
+            this.btnDashboard.Location = new System.Drawing.Point(0, 155);
+            this.btnDashboard.Margin = new System.Windows.Forms.Padding(0);
+            this.btnDashboard.Name = "btnDashboard";
+            this.btnDashboard.Size = new System.Drawing.Size(394, 75);
+            this.btnDashboard.TabIndex = 0;
+            this.btnDashboard.Text = "Dashboard";
+            this.btnDashboard.UseVisualStyleBackColor = true;
+            // 
+            // btnIncomeManagement
+            // 
+            this.btnIncomeManagement.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIncomeManagement.FlatAppearance.BorderSize = 0;
+            this.btnIncomeManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIncomeManagement.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIncomeManagement.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(244)))), ((int)(((byte)(246)))));
+            this.btnIncomeManagement.Location = new System.Drawing.Point(0, 230);
+            this.btnIncomeManagement.Margin = new System.Windows.Forms.Padding(0);
+            this.btnIncomeManagement.Name = "btnIncomeManagement";
+            this.btnIncomeManagement.Size = new System.Drawing.Size(394, 75);
+            this.btnIncomeManagement.TabIndex = 1;
+            this.btnIncomeManagement.Text = "Income Management";
+            this.btnIncomeManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnExpensesManagement
+            // 
+            this.btnExpensesManagement.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExpensesManagement.FlatAppearance.BorderSize = 0;
+            this.btnExpensesManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExpensesManagement.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExpensesManagement.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(244)))), ((int)(((byte)(246)))));
+            this.btnExpensesManagement.Location = new System.Drawing.Point(0, 305);
+            this.btnExpensesManagement.Margin = new System.Windows.Forms.Padding(0);
+            this.btnExpensesManagement.Name = "btnExpensesManagement";
+            this.btnExpensesManagement.Size = new System.Drawing.Size(394, 75);
+            this.btnExpensesManagement.TabIndex = 2;
+            this.btnExpensesManagement.Text = "Expenses Management";
+            this.btnExpensesManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnSavings
+            // 
+            this.btnSavings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSavings.FlatAppearance.BorderSize = 0;
+            this.btnSavings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSavings.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSavings.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(244)))), ((int)(((byte)(246)))));
+            this.btnSavings.Location = new System.Drawing.Point(0, 380);
+            this.btnSavings.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSavings.Name = "btnSavings";
+            this.btnSavings.Size = new System.Drawing.Size(394, 75);
+            this.btnSavings.TabIndex = 3;
+            this.btnSavings.Text = "Savings";
+            this.btnSavings.UseVisualStyleBackColor = true;
+            // 
+            // btnReports
+            // 
+            this.btnReports.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReports.FlatAppearance.BorderSize = 0;
+            this.btnReports.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReports.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReports.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(244)))), ((int)(((byte)(246)))));
+            this.btnReports.Location = new System.Drawing.Point(0, 455);
+            this.btnReports.Margin = new System.Windows.Forms.Padding(0);
+            this.btnReports.Name = "btnReports";
+            this.btnReports.Size = new System.Drawing.Size(394, 75);
+            this.btnReports.TabIndex = 4;
+            this.btnReports.Text = "Reports";
+            this.btnReports.UseVisualStyleBackColor = true;
+            // 
+            // btnCategoryList
+            // 
+            this.btnCategoryList.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCategoryList.FlatAppearance.BorderSize = 0;
+            this.btnCategoryList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCategoryList.Font = new System.Drawing.Font("Montserrat", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCategoryList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(244)))), ((int)(((byte)(246)))));
+            this.btnCategoryList.Location = new System.Drawing.Point(0, 530);
+            this.btnCategoryList.Margin = new System.Windows.Forms.Padding(0);
+            this.btnCategoryList.Name = "btnCategoryList";
+            this.btnCategoryList.Size = new System.Drawing.Size(394, 75);
+            this.btnCategoryList.TabIndex = 5;
+            this.btnCategoryList.Text = "Category List";
+            this.btnCategoryList.UseVisualStyleBackColor = true;
+            // 
+            // picDarkMode
+            // 
+            this.picDarkMode.BackColor = System.Drawing.Color.Transparent;
+            this.picDarkMode.Image = ((System.Drawing.Image)(resources.GetObject("picDarkMode.Image")));
+            this.picDarkMode.Location = new System.Drawing.Point(21, 1027);
+            this.picDarkMode.Name = "picDarkMode";
+            this.picDarkMode.Size = new System.Drawing.Size(47, 50);
+            this.picDarkMode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picDarkMode.TabIndex = 6;
+            this.picDarkMode.TabStop = false;
+            // 
+            // picHelp
+            // 
+            this.picHelp.BackColor = System.Drawing.Color.Transparent;
+            this.picHelp.Image = ((System.Drawing.Image)(resources.GetObject("picHelp.Image")));
+            this.picHelp.Location = new System.Drawing.Point(253, 1025);
+            this.picHelp.Name = "picHelp";
+            this.picHelp.Size = new System.Drawing.Size(48, 52);
+            this.picHelp.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picHelp.TabIndex = 7;
+            this.picHelp.TabStop = false;
+            // 
+            // picSettings
+            // 
+            this.picSettings.BackColor = System.Drawing.Color.Transparent;
+            this.picSettings.Image = ((System.Drawing.Image)(resources.GetObject("picSettings.Image")));
+            this.picSettings.Location = new System.Drawing.Point(307, 1027);
+            this.picSettings.Name = "picSettings";
+            this.picSettings.Size = new System.Drawing.Size(65, 46);
+            this.picSettings.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picSettings.TabIndex = 8;
+            this.picSettings.TabStop = false;
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(244)))), ((int)(((byte)(246)))));
+            this.topPanel.Controls.Add(this.leftCornerLogo);
+            this.topPanel.Location = new System.Drawing.Point(3, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(1500, 115);
+            this.topPanel.TabIndex = 1;
+            // 
+            // leftCornerLogo
+            // 
+            this.leftCornerLogo.BackColor = System.Drawing.Color.Transparent;
+            this.leftCornerLogo.Image = ((System.Drawing.Image)(resources.GetObject("leftCornerLogo.Image")));
+            this.leftCornerLogo.Location = new System.Drawing.Point(6, 9);
+            this.leftCornerLogo.Margin = new System.Windows.Forms.Padding(0);
+            this.leftCornerLogo.Name = "leftCornerLogo";
+            this.leftCornerLogo.Size = new System.Drawing.Size(119, 92);
+            this.leftCornerLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.leftCornerLogo.TabIndex = 0;
+            this.leftCornerLogo.TabStop = false;
+            // 
+            // mainFormTitle
+            // 
+            this.mainFormTitle.AutoSize = true;
+            this.mainFormTitle.Font = new System.Drawing.Font("Montserrat SemiBold", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainFormTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(65)))), ((int)(((byte)(81)))));
+            this.mainFormTitle.Location = new System.Drawing.Point(148, 38);
+            this.mainFormTitle.Name = "mainFormTitle";
+            this.mainFormTitle.Size = new System.Drawing.Size(586, 45);
+            this.mainFormTitle.TabIndex = 1;
+            this.mainFormTitle.Text = "Income and Expenses Tracker System";
+            this.mainFormTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainForm";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(244)))), ((int)(((byte)(246)))));
+            this.ClientSize = new System.Drawing.Size(1505, 1099);
+            this.Controls.Add(this.mainFormTitle);
+            this.Controls.Add(this.sidebarPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Income and Expenses Tracker System";
+            this.sidebarPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picDarkMode)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHelp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSettings)).EndInit();
+            this.topPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.leftCornerLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel sidebarPanel;
+        private System.Windows.Forms.Button btnDashboard;
+        private System.Windows.Forms.Button btnCategoryList;
+        private System.Windows.Forms.Button btnReports;
+        private System.Windows.Forms.Button btnSavings;
+        private System.Windows.Forms.Button btnExpensesManagement;
+        private System.Windows.Forms.Button btnIncomeManagement;
+        private System.Windows.Forms.PictureBox picSettings;
+        private System.Windows.Forms.PictureBox picHelp;
+        private System.Windows.Forms.PictureBox picDarkMode;
+        private System.Windows.Forms.Panel topPanel;
+        private System.Windows.Forms.PictureBox leftCornerLogo;
+        private System.Windows.Forms.Label mainFormTitle;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -16,5 +16,10 @@ namespace IncomeExpensesTrackingSystem
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
