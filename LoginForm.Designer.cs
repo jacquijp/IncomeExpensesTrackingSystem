e8496545c93b40159d191a548705be5ddae75c2a
Jacqueline Jimenez <jacquelinejp2019@gmail.com>
2025-02-23 16:44:05 +0100

    LoginForm changes

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -30,101 +30,148 @@
             this.label5 = new System.Windows.Forms.Label();
             this.login_button = new System.Windows.Forms.Button();
             this.login_showPassword = new System.Windows.Forms.CheckBox();
-
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-
-            // Panel Left
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(10, 61, 98);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(61)))), ((int)(((byte)(98)))));
             this.panel1.Controls.Add(this.login_signupButton);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(625, 1013);
-
-            // Sign Up Button
-            this.login_signupButton.BackColor = System.Drawing.Color.FromArgb(59, 130, 246);
+            this.panel1.TabIndex = 8;
+            // 
+            // login_signupButton
+            // 
+            this.login_signupButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(130)))), ((int)(((byte)(246)))));
             this.login_signupButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.login_signupButton.Font = new System.Drawing.Font("Montserrat", 10.875F);
             this.login_signupButton.ForeColor = System.Drawing.Color.White;
             this.login_signupButton.Location = new System.Drawing.Point(45, 904);
+            this.login_signupButton.Name = "login_signupButton";
             this.login_signupButton.Size = new System.Drawing.Size(527, 76);
+            this.login_signupButton.TabIndex = 0;
             this.login_signupButton.Text = "SIGNUP";
             this.login_signupButton.UseVisualStyleBackColor = false;
             this.login_signupButton.Click += new System.EventHandler(this.login_signupButton_Click);
-
-            // App Title Label
+            // 
+            // label2
+            // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold);
             this.label2.ForeColor = System.Drawing.Color.Gainsboro;
-            this.label2.Location = new System.Drawing.Point(52, 481);
+            this.label2.Location = new System.Drawing.Point(36, 483);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(538, 49);
+            this.label2.TabIndex = 1;
             this.label2.Text = "Income and Expenses Tracker";
-
-            // Logo
+            // 
+            // pictureBox1
+            // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(181, 194);
+            this.pictureBox1.Location = new System.Drawing.Point(243, 361);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(133, 97);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-
-            // Close Button
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // close_Click
+            // 
             this.close_Click.AutoSize = true;
             this.close_Click.Cursor = System.Windows.Forms.Cursors.Hand;
             this.close_Click.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
             this.close_Click.Location = new System.Drawing.Point(1225, 28);
+            this.close_Click.Name = "close_Click";
+            this.close_Click.Size = new System.Drawing.Size(29, 29);
+            this.close_Click.TabIndex = 7;
             this.close_Click.Text = "X";
             this.close_Click.Click += new System.EventHandler(this.close_Click_Click);
-
-            // Sign In Label
+            // 
+            // signin_label
+            // 
             this.signin_label.AutoSize = true;
             this.signin_label.Font = new System.Drawing.Font("Montserrat SemiBold", 16.125F, System.Drawing.FontStyle.Bold);
-            this.signin_label.ForeColor = System.Drawing.Color.FromArgb(59, 65, 81);
+            this.signin_label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(65)))), ((int)(((byte)(81)))));
             this.signin_label.Location = new System.Drawing.Point(648, 224);
+            this.signin_label.Name = "signin_label";
+            this.signin_label.Size = new System.Drawing.Size(199, 67);
+            this.signin_label.TabIndex = 2;
             this.signin_label.Text = "SIGN IN";
-
-            // Username Label
+            // 
+            // label4
+            // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Montserrat", 12F);
-            this.label4.ForeColor = System.Drawing.Color.FromArgb(59, 65, 81);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(65)))), ((int)(((byte)(81)))));
             this.label4.Location = new System.Drawing.Point(661, 380);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 49);
+            this.label4.TabIndex = 5;
             this.label4.Text = "Username";
-
-            // Username Input
+            // 
+            // signin_username
+            // 
             this.signin_username.Font = new System.Drawing.Font("Lucida Sans", 10.125F);
             this.signin_username.Location = new System.Drawing.Point(660, 439);
+            this.signin_username.Name = "signin_username";
             this.signin_username.Size = new System.Drawing.Size(576, 39);
-
-            // Password Label
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Montserrat", 12F);
-            this.label5.ForeColor = System.Drawing.Color.FromArgb(59, 65, 81);
-            this.label5.Location = new System.Drawing.Point(663, 510);
-            this.label5.Text = "Password";
-
-            // Password Input
+            this.signin_username.TabIndex = 3;
+            // 
+            // login_password
+            // 
             this.login_password.Location = new System.Drawing.Point(660, 570);
+            this.login_password.Name = "login_password";
             this.login_password.PasswordChar = '*';
             this.login_password.Size = new System.Drawing.Size(576, 31);
-
-            // Login Button
-            this.login_button.BackColor = System.Drawing.Color.FromArgb(59, 130, 246);
+            this.login_password.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Montserrat", 12F);
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(65)))), ((int)(((byte)(81)))));
+            this.label5.Location = new System.Drawing.Point(663, 510);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(179, 49);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Password";
+            // 
+            // login_button
+            // 
+            this.login_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(130)))), ((int)(((byte)(246)))));
             this.login_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.login_button.Font = new System.Drawing.Font("Montserrat", 10.875F);
             this.login_button.ForeColor = System.Drawing.Color.White;
             this.login_button.Location = new System.Drawing.Point(670, 694);
+            this.login_button.Name = "login_button";
             this.login_button.Size = new System.Drawing.Size(173, 76);
+            this.login_button.TabIndex = 1;
             this.login_button.Text = "LOGIN";
             this.login_button.UseVisualStyleBackColor = false;
             this.login_button.Click += new System.EventHandler(this.login_button_Click);
-
-            // Show Password Checkbox
+            // 
+            // login_showPassword
+            // 
             this.login_showPassword.AutoSize = true;
             this.login_showPassword.Font = new System.Drawing.Font("Montserrat", 7.875F);
-            this.login_showPassword.ForeColor = System.Drawing.Color.FromArgb(59, 65, 81);
-            this.login_showPassword.Location = new System.Drawing.Point(1025, 620);
+            this.login_showPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(65)))), ((int)(((byte)(81)))));
+            this.login_showPassword.Location = new System.Drawing.Point(1033, 620);
+            this.login_showPassword.Name = "login_showPassword";
+            this.login_showPassword.Size = new System.Drawing.Size(211, 37);
+            this.login_showPassword.TabIndex = 0;
             this.login_showPassword.Text = "Show Password";
             this.login_showPassword.CheckedChanged += new System.EventHandler(this.login_showPassword_CheckedChanged);
-
-            // Form Settings
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1279, 1013);
             this.Controls.Add(this.login_showPassword);
             this.Controls.Add(this.login_button);
@@ -136,7 +183,14 @@
             this.Controls.Add(this.close_Click);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
             this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
